b05496a3c811292b671fce4e12f59b85f619b0e0
Finpi <finpi.app@gmail.com>
2025-11-14 22:00:35 +0100

    Update metadata.js

--- api/metadata.js
+++ api/metadata.js
@@ -1,9 +1,8 @@
-// api/metadata.js
 export default function handler(req, res) {
   res.status(200).json({
     name: "Finpi",
     description: "Finpi — secure Testnet fintech powered by Pi Network.",
     icon: "https://finpi.vercel.app/logo.png",
-    permissions: ["payments"],
+    permissions: ["payments"]
   });
 }
