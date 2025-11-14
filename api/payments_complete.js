b6640a7adbe800e5f92dfac65f1e2f56aa34d3ef
Finpi <finpi.app@gmail.com>
2025-11-14 06:40:42 +0100

    Add payments_complete Endpoint

---
+++ api/payments_complete.js
@@ -0,0 +1,11 @@
+// api/payments_complete.js
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+  const { paymentId, txid } = req.body || {};
+  console.log("Payment complete:", { paymentId, txid });
+
+  // TODO: save to DB / mark transaction complete
+  return res.status(200).json({ success: true, paymentId, txid });
+}
